2bc157dba5060bed930e9f84b7fb3033c56f3d6e
TeacherArea <sewilius.berg@lbs.se>
2023-12-17 19:58:36 +0100

    Add project files.

---
+++ PRG1_Manage_UserInputs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PRG1_Manage_UserInputs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            TestANumber();
+        }
+        static void TestANumber()
+        {
+            Console.WriteLine("Hello World!");
+            string? input;
+            bool run = true;
+            while (run)
+            {
+                Console.Write("Feed me with a number, please: ");
+                input = Console.ReadLine();
+                if (!double.TryParse(input, out double correct) || String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine($"Nope. Not a correct number. Try Again! ");
+                }
+                else
+                {
+                    Console.WriteLine($"Thanks. The number is {correct}");
+                    Console.Write("Feed me again? [y/n]");
+                    string? goAgain = Console.ReadLine();
+                    if (goAgain == "n" || goAgain == "N")
+                    {
+                        Console.WriteLine("Ok. But remember me when you come across a number, yes? Bye!");
+                        run = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Yes, eyes... numbers is da best!");
+                    }
+                }
+            }
+        }
+    }
+}
